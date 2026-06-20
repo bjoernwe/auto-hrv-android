87c7f63f5e38b855b15f07d6858629b1b923461d
Björn <bjoernwe@users.noreply.github.com>
2026-06-20 10:55:16 +0200

    Update config

--- app/src/main/java/dev/upaya/autohrv/domain/breathing/BreathingConfig.kt
+++ app/src/main/java/dev/upaya/autohrv/domain/breathing/BreathingConfig.kt
@@ -4,14 +4,16 @@ data class BreathingConfig(
     val evaluationLengthSeconds: Int,
     val outToInRatio: Float,
     val cycleLength: Float,
-    val cycleLengthRange: ClosedFloatingPointRange<Float>,
+    val initialCycleLengthRange: ClosedFloatingPointRange<Float>,
+    val maxCycleLengthRange: ClosedFloatingPointRange<Float>,
 ) {
     companion object {
         val DEFAULT = BreathingConfig(
             evaluationLengthSeconds = 32+1, // 2^n + 1 is a good value for the power spectrum
             outToInRatio = 1f,
             cycleLength = 8f,
-            cycleLengthRange = 5f.rangeTo(11f),
+            initialCycleLengthRange = 6f..10f,
+            maxCycleLengthRange = 4f..20f,
         )
     }
 }
