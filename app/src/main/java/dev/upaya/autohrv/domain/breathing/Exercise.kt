fa3fb60f4fbdd8298b5fd8dadc41d06983c97342
Björn <bjoernwe@users.noreply.github.com>
2026-08-29 21:32:59 +0200

    adjust experiment ranges

--- app/src/main/java/dev/upaya/autohrv/domain/breathing/Exercise.kt
+++ app/src/main/java/dev/upaya/autohrv/domain/breathing/Exercise.kt
@@ -7,9 +7,9 @@ data class Exercise(
     companion object {
         val all: List<Exercise> =
             listOf(
-                Exercise("Fast", 3..5),
+                Exercise("Fast", 4..5),
                 Exercise("Medium", 6..7),
-                Exercise("Slow", 8..12),
+                Exercise("Slow", 9..12),
                 Exercise("Free", BreathingConfig.DEFAULT.maxCycleLengthRange),
             )
     }
