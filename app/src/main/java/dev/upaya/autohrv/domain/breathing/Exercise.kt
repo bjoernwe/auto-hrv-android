0fd8f3ce15c64b1b88449754fc866ec870be9c0e
Björn <bjoernwe@users.noreply.github.com>
2026-08-30 11:39:50 +0200

    update label

--- app/src/main/java/dev/upaya/autohrv/domain/breathing/Exercise.kt
+++ app/src/main/java/dev/upaya/autohrv/domain/breathing/Exercise.kt
@@ -8,7 +8,7 @@ data class Exercise(
         val all: List<Exercise> =
             listOf(
                 Exercise("Fast", 4..5),
-                Exercise("Medium", 6..7),
+                Exercise("Med.", 6..7),
                 Exercise("Slow", 9..12),
                 Exercise("Free", BreathingConfig.DEFAULT.maxCycleLengthRange),
             )
